d8d6e0de828cd57b4bde6348686c80d523c19fb3
Gwennael Jean <gwennael.jean@trsb.net>
2017-03-09 17:59:31 +0100

    chore: ajout de la class abstraite ServiceProvider

---
+++ src/ServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tourbillon\ServiceContainer;
+
+/**
+ * Description of ServiceProvider
+ *
+ * @author gjean
+ */
+abstract class ServiceProvider implements ConfigurableServiceInterface
+{
+    
+}
